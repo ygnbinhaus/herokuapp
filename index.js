3dc335a0c9e6cbc245f0d204658689b4d83aa8d0
nyinyinyanlin <nyinyinyanlin.mm@gmail.com>
2017-02-28 00:51:53 +0630

    route added

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.set('port', (process.env.PORT || 5000));
+app.get('/',function(req,res){
+	res.send('Hello Heroku')
+	})
+app.listen(app.get('port'),function(err){
+	if (err) {
+		return console.log("An error occured",err)
+	}
+	console.log('Example app listening on port 5000')
+	})
